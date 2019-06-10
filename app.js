52a11cd6e40b5c3fb08aa94d64f43d77fb77b38c
Dorothy <uxdesigndorothy@gmail.com>
2019-06-10 03:38:27 +0000

    assignment 2589 completed

--- app.js
+++ app.js
@@ -1,30 +1,54 @@
 function onReady() {
   const toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
+  let id = 0;
 
   function createNewToDo() {
     const newToDoText = document.getElementById('newToDoText');
     if (!newToDoText.value) { return; }
+
     toDos.push({
       title: newToDoText.value,
-      complete: false
+      complete: false,
+      id: id
     });
+
+    id++;
+
     newToDoText.value = '';
+
     renderTheUI();
-  }
+  };
 
   function renderTheUI() {
     const toDoList = document.getElementById('toDoList');
+
     toDoList.textContent = '';
+
     toDos.forEach(function(toDo) {
       const newLi = document.createElement('li');
       const checkbox = document.createElement('input');
       checkbox.type = "checkbox";
+
+      const deleteBtn = document.createElement('Button');
+      deleteBtn.textContent = "Delete!";
+
+      deleteBtn.addEventListener('click', event => {
+        toDos = toDos.filter(function(toDo){
+          return item.id !== toDo.id;
+          toDoList.removeChild(this.parentElement);
+        })
+        renderTheUI();
+      });
+
       newLi.textContent = toDo.title;
-      toDoList.appendChild(newLi);
+
       newLi.appendChild(checkbox);
+      toDoList.appendChild(newLi);
+      newLi.appendChild(deleteBtn);
+
     });
-  }
+  };
 
   addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
@@ -32,7 +56,7 @@ function onReady() {
   });
 
   renderTheUI();
-}
+};
 
 window.onload = function() {
   onReady();
